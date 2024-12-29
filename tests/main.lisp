cfefe93ee65972e946f649955eabc44e73f58c60
NMunro <neilmunro@gmail.com>
2024-12-29 19:15:41 +0000

    Renamed package

--- tests/main.lisp
+++ tests/main.lisp
@@ -1,10 +1,10 @@
-(defpackage barghest-routes/tests/main
+(defpackage barghest/routes/tests/main
   (:use :cl
-        :barghest-routes
+        :barghest/routes
         :rove))
-(in-package :barghest-routes/tests/main)
+(in-package :barghest/routes/tests/main)
 
-;; NOTE: To run this test file, execute `(asdf:test-system :barghest-routes)' in your Lisp.
+;; NOTE: To run this test file, execute `(asdf:test-system :barghest/routes)' in your Lisp.
 
 (deftest test-target-1
   (testing "should (= 1 1) to be true"
